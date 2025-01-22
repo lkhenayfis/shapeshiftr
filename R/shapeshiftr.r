49cda2798eeb7bd63c1d6c092939e68caaa3ebd6
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-01-22 09:59:38 -0300

    Init repo

---
+++ R/shapeshiftr.r
@@ -0,0 +1,14 @@
+#' PAckage \code{shapeshiftr}
+#' 
+#' Automated pipeline for shape manipulation and data transforms
+#' 
+#' This package intends to unify several kinds of data wrangling under a single framework.
+#' This includes shape manipulation as well data transformations. All is meant to be easily
+#' readable and extensible, aiming at greatly simplifying the process of data preparation before
+#' fitting/using models.
+#' 
+#' @docType package
+#' @name shapeshiftR
+#' 
+#' @import stats utils data.table magrittr
+NULL
