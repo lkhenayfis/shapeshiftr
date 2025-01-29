99541e90b6a9d5d243995c01e536cbdc5ed7d31c
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-01-29 15:17:05 -0300

    Modifications in keyed dts

--- data-raw/build_keyed_dts.r
+++ data-raw/build_keyed_dts.r
@@ -1,6 +1,6 @@
 library(data.table)
 
-set.seed(1234)
+set.seed(1235)
 
 date0 <- as.Date("2025-01-01")
 keyed_dt_date <- data.table(
@@ -16,7 +16,7 @@ usethis::use_data(keyed_dt_date, overwrite = TRUE)
 datetime0 <- as.POSIXct("2025-01-01 00:00:00", "GMT")
 keyed_dt_datetime <- data.table(
     datetime = datetime0 + rep(seq(1, 20) * 3600, each = 5),
-    target_datetime = datetime0 + rep(seq(1, 20) * 3600, each = 5) + rep(seq(1, 5) * 3600, 20),
+    target_datetime = datetime0 + rep(seq(1, 20) * 3600, each = 5) + rep(seq(1, 5) * 1800, 20),
     X1 = sample(1:100, 100),
     X2 = sample(-100:-1, 100),
     X3 = sample(50:150, 100),
