79e8928016ffa182ffc0ef1ba8d4f41185add021
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-02-14 17:32:14 -0300

    Adds functionality to refresh cluster each use

--- R/utils.r
+++ R/utils.r
@@ -1,4 +1,21 @@
 
+get_cluster <- function() get(".SHAPESHIFTR_CLUSTER", envir = asNamespace("shapeshiftr"))
+
+refresh_cluster <- function(cl, envir) UseMethod("refresh_cluster")
+
+refresh_cluster.default <- function(cl, envir) invisible(NULL)
+
+refresh_cluster.cluster <- function(cl, envir) {
+    vars <- ls(envir = envir)
+    parallel::clusterExport(cl, vars, envir)
+}
+
+run_loop <- function(X, fun) {
+    cl <- get_cluster()
+    refresh_cluster(cl, parent.frame())
+    inner_run(cl, X, fun)
+}
+
 inner_run <- function(cl, X, fun) UseMethod("inner_run")
 
 inner_run.default <- function(cl, X, fun) lapply(X, fun)
