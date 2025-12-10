b7b87595f1bad23cd092b707f816160b8634c178
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-12-10 11:24:59 -0300

    Add teste de eval_single_pipe

--- tests/testthat/test-pipes.r
+++ tests/testthat/test-pipes.r
@@ -1,20 +1,20 @@
-test_that("parse_single_pipe", {
+gen_closure_filter <- function(by, value, ...) function(x) x[x[[by]] == value, ]
+gen_closure_summary <- function(...) function(x) summary(x)
 
-    gen_closure_filter <- function(by, value, ...) function(x) x[x[[by]] == value, ]
-    gen_closure_summary <- function(...) function(x) summary(x)
-
-    raw_pipe <- list(
-        on = "mtcars",
-        transforms = list(
-            list(
-                fun = "gen_closure_filter",
-                by = "cyl", value = 6
-            ),
-            list(
-                fun = "gen_closure_summary"
-            )
+raw_pipe <- list(
+    on = "mtcars",
+    transforms = list(
+        list(
+            fun = "gen_closure_filter",
+            by = "cyl", value = 6
+        ),
+        list(
+            fun = "gen_closure_summary"
         )
     )
+)
+
+test_that("parse_single_pipe", {
 
     parsed_pipe <- parse_single_pipe(raw_pipe)
 
@@ -34,4 +34,14 @@ test_that("parse_single_pipe", {
 
     expect_equal(deparse(body(l_t[[1]])), "x[x[[by]] == value, ]")
     expect_equal(deparse(body(l_t[[2]])), "summary(x)")
+})
+
+test_that("eval_single_pipe", {
+    parsed_pipe <- parse_single_pipe(raw_pipe)
+
+    eval_pipe <- eval_single_pipe(parsed_pipe)
+
+    expect_true(inherits(eval_pipe, "table"))
+    expect_equal(eval_pipe[1, 1], "Min.   :17.80  ")
+    expect_equal(eval_pipe[1, 2], "Min.   :6  ")
 })
