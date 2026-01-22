eda201a3cb9a82fadb2b401e45211969349f3b09
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2026-01-22 11:27:12 -0300

    Add categorical cols to simple_dts

--- R/data.r
+++ R/data.r
@@ -14,20 +14,32 @@
 #' In the following, all values are merely illustrative and have no bearing on the actual datasets.
 #' 
 #' The ones prefixed \code{simple} have a single column of time index, which can be of either type
-#' \code{Date} or \code{POSIXct}, as indicaded by the suffixes. \code{simple_dt_date} looks like
+#' \code{Date} or \code{POSIXct}, as indicated by the suffixes. They also include categorical
+#' variables to demonstrate mixed-type data workflows.
 #' 
-#' |      date  | X1  |  X2  | X3  |   Y |
-#' |      ---   | --- |  --- | --- | --- |
-#' | 2025-01-02 | 16  | -18  | 66  | 132 |
-#' | 2025-01-03 |  5  | -17  | 55  | 122 |
-#' | 2025-01-04 | 12  | -16  | 68  | 138 |
-#' | 2025-01-05 | 15  | -19  | 69  | 137 |
-#' | 2025-01-06 |  9  |  -7  | 70  | 125 |
+#' \code{simple_dt_date} looks like:
+#' 
+#' |      date  | X1  |  X2  | X3  |   Y | day_type | weather_condition |
+#' |      ---   | --- |  --- | --- | --- | ---      | ---               |
+#' | 2025-01-02 | 16  | -18  | 66  | 132 | weekday  | sunny             |
+#' | 2025-01-03 |  5  | -17  | 55  | 122 | weekday  | cloudy            |
+#' | 2025-01-04 | 12  | -16  | 68  | 138 | weekend  | rainy             |
+#' | 2025-01-05 | 15  | -19  | 69  | 137 | weekend  | sunny             |
+#' | 2025-01-06 |  9  |  -7  | 70  | 125 | weekday  | cloudy            |
+#' 
+#' The categorical columns are:
+#' \itemize{
+#' \item \code{day_type}: Factor with levels \code{"weekday"} and \code{"weekend"},
+#'   determined by the day of the week. Used to demonstrate categorical variable handling.
+#' \item \code{weather_condition}: Factor with levels \code{"sunny"}, \code{"cloudy"},
+#'   and \code{"rainy"}, randomly assigned. Used to demonstrate multi-level factor encoding.
+#' }
 #' 
 #' \code{simple_dt_datetime} is similar, except instead of column \code{date}, there is a column
-#' named \code{datetime} of hour by hour times.
+#' named \code{datetime} of hour by hour times. The \code{day_type} column for this dataset
+#' has levels \code{"work_hours"} (9am-5pm) and \code{"off_hours"}, based on the hour of day.
 #' 
-#' Keyed datasets are very similar, except for having an aditional time index column. For example,
+#' Keyed datasets are very similar, except for having an additional time index column. For example,
 #' \code{keyed_dt_date} looks like
 #' 
 #' |      date  | target_date | X1  |  X2  | X3  |   Y |
--- data-raw/build_simple_dts.r
+++ data-raw/build_simple_dts.r
@@ -3,22 +3,53 @@ library(data.table)
 set.seed(1234)
 
 date0 <- as.Date("2025-01-01")
+dates <- date0 + 1:20
+
 simple_dt_date <- data.table(
-    date = date0 + 1:20,
+    date = dates,
     X1 = sample(1:20, 20),
     X2 = sample(-20:-1, 20),
     X3 = sample(50:70, 20),
     Y = sample(120:140, 20)
 )
+
+simple_dt_date[, day_type := factor(
+    ifelse(weekdays(date) %in% c("Saturday", "Sunday"), "weekend", "weekday"),
+    levels = c("weekday", "weekend")
+)]
+
+set.seed(5678)
+simple_dt_date[, weather_condition := factor(
+    sample(c("sunny", "cloudy", "rainy"), 20, replace = TRUE),
+    levels = c("sunny", "cloudy", "rainy")
+)]
+
 usethis::use_data(simple_dt_date, overwrite = TRUE)
 
+set.seed(1234)
+
 datetime0 <- as.POSIXct("2025-01-01 00:00:00", "GMT")
+datetimes <- datetime0 + seq(1, 20) * 3600
+
 simple_dt_datetime <- data.table(
-    datetime = datetime0 + seq(1, 20) * 3600,
+    datetime = datetimes,
     X1 = sample(1:20, 20),
     X2 = sample(-20:-1, 20),
     X3 = sample(50:70, 20),
     Y = sample(120:140, 20)
 )
+
+hours <- as.POSIXlt(datetimes)$hour
+simple_dt_datetime[, day_type := factor(
+    ifelse(hours >= 9 & hours < 18, "work_hours", "off_hours"),
+    levels = c("work_hours", "off_hours")
+)]
+
+set.seed(5678)
+simple_dt_datetime[, weather_condition := factor(
+    sample(c("sunny", "cloudy", "rainy"), 20, replace = TRUE),
+    levels = c("sunny", "cloudy", "rainy")
+)]
+
 usethis::use_data(simple_dt_datetime, overwrite = TRUE)
 
